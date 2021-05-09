6528245a2a0fca3b3caaabdc463ebb34a870b53b
SUKHWANT <SUKHWANT@DESKTOP-VLRU9PR>
2021-05-09 15:37:23 -0400

    trying to push changes into remote repo

---
+++ 2021April/src/day2/java/Arrays.java
@@ -0,0 +1,17 @@
+package day2.java;
+
+public class Arrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] price = {30,40,50,60,65};
+		for(int i = 0;i<5;i++){
+		System.out.println(price[i]);
+		}
+		for(int i: price){
+			System.out.println(i);
+			
+		}
+	}
+
+}
